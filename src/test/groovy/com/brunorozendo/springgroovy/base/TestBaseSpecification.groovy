83171a0e298a8b4b1b4b9c04bc07c3c2d6cde179
Bruno Rozendo <bruno@brunorozendo.com>
2019-02-05 17:43:37 -0200

    clean config

--- src/test/groovy/com/brunorozendo/springgroovy/base/TestBaseSpecification.groovy
+++ src/test/groovy/com/brunorozendo/springgroovy/base/TestBaseSpecification.groovy
@@ -1,11 +1,10 @@
 package com.brunorozendo.springgroovy.base
 
 import com.brunorozendo.springgroovy.base.config.TestBaseConfiguration
-import org.springframework.boot.test.context.SpringBootContextLoader
 import org.springframework.test.context.ContextConfiguration
 import spock.lang.Specification
 
-@ContextConfiguration(loader = SpringBootContextLoader, classes = TestBaseConfiguration)
+@ContextConfiguration(classes = TestBaseConfiguration)
 class TestBaseSpecification extends Specification{
 
 }
--- src/test/groovy/com/brunorozendo/springgroovy/base/config/TestBaseConfiguration.groovy
+++ src/test/groovy/com/brunorozendo/springgroovy/base/config/TestBaseConfiguration.groovy
@@ -1,24 +1,13 @@
 package com.brunorozendo.springgroovy.base.config
 
-import com.brunorozendo.springgroovy.SpringGroovyApplication
 import com.brunorozendo.springgroovy.service.PessoaService
-import org.spockframework.spring.ScanScopedBeans
-import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
-import org.springframework.test.context.ContextConfiguration
-import org.springframework.test.context.TestPropertySource
 import spock.mock.DetachedMockFactory
 
-@SpringBootTest
-@ContextConfiguration(classes = [SpringGroovyApplication])
 @TestConfiguration
-@ScanScopedBeans
 @ComponentScan(basePackages = ['com.brunorozendo.springgroovy'])
-@TestPropertySource(locations = [
-    'classpath:/config/application.yaml',
-    'classpath:/config/application-test.yaml'])
 class TestBaseConfiguration{
 
   DetachedMockFactory mockFactory = new DetachedMockFactory()
