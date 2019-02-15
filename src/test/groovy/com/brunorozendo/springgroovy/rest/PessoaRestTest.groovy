ffa47558e0a4fd17c9afa27cd65d288831b3f3f7
Bruno Rozendo <bruno@brunorozendo.com>
2019-02-15 02:05:12 -0200

    troca das labels para ficar em português

--- src/test/groovy/com/brunorozendo/springgroovy/rest/PessoaRestTest.groovy
+++ src/test/groovy/com/brunorozendo/springgroovy/rest/PessoaRestTest.groovy
@@ -1,7 +1,12 @@
 package com.brunorozendo.springgroovy.rest
 
 import com.brunorozendo.springgroovy.base.TestBaseSpecification
+
 import com.brunorozendo.springgroovy.service.PessoaService
+
+import spock.lang.Narrative
+import spock.lang.Title
+
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
@@ -13,6 +18,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
+@Title("This is easy to read")
+@Narrative("""
+As a user
+I want foo
+So that bar
+""")
 class PessoaRestTest extends TestBaseSpecification{
 
   @Autowired
@@ -24,12 +35,17 @@ class PessoaRestTest extends TestBaseSpecification{
   MockMvc mockMvc
 
 
+  def setupSpec() {
+	  reportHeader "<h2>Algum nome relevante</h2>"
+  }
+  
   void setup() {
     this.pessoaRest.pessoaService = pessoaServiceTest
     this.mockMvc = MockMvcBuilders.standaloneSetup(pessoaRest).build()
   }
 
   def 'pesquisar por unidade chama o serviço e retorna como JSON'() {
+    reportInfo "Some information I want to show in the report"
     given: "mock do retorno na pessoa"
     1 * pessoaServiceTest.pessoa >> {
       def pessoaDto = new PessoaDto()
@@ -41,11 +57,11 @@ class PessoaRestTest extends TestBaseSpecification{
     def response = mockMvc.perform(get("/api/pessoa/10"))
 
     then: "deve retornar"
-	and: " - página com estatus 200 (OK)"
+	and: "página com estatus 200 (OK)"
     response.andExpect(status().isOk())
-	and: " - o tipo da resposta deve ser json"
+	and: "o tipo da resposta deve ser json"
     response.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
-	and: " - o nome deve ser igual a 'ajajajaj'"
+	and: "o nome deve ser igual a 'ajajajaj'"
     response.andExpect(jsonPath('$.name', is('jajajaa')))
 
   }
